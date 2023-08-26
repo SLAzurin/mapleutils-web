bfa83d73e9445d61f0fefff176a0163ab9a38b5e
SLAzurin <69220163+SLAzurin@users.noreply.github.com>
2023-08-26 04:58:55 -0400

    Release 3

--- _next/static/chunks/app/[lng]/page-d8532a8cc9a88323.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2866],{3965:function(e,n,s){Promise.resolve().then(s.bind(s,1840)),Promise.resolve().then(s.bind(s,6315))}},function(e){e.O(0,[8011,87,9805,6315,2667,8139,1744],function(){return e(e.s=3965)}),_N_E=e.O()}]);
